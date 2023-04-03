e71a9c20a1da8902c70ef21f8e2d11de5c4ccd4f
nguyenhuuhuy <huy.cb96@gmail.com>
2023-04-03 08:50:07 +0700

    bt

---
+++ src/demo/bt3/CrawlNews.java
@@ -0,0 +1,39 @@
+package demo.bt3;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlNews {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            scanner.close();
+            content = content.replaceAll("\\n+", "");
+            Pattern p = Pattern.compile("article-title\">(.*?)</h3>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                String text = m.group(1);
+                String[] textArr = text.split(".htm\">");
+                String finalText = "";
+                finalText = textArr[1].replaceAll("</a>",".");
+                finalText = finalText.replaceAll("&quot;", "\"");
+                System.out.println(finalText);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
